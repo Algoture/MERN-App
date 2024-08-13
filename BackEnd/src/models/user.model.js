cbae11b7d094c2f829dc917a603e7138cb69ba16
Umesh Nagare <umeshnagare27@gmail.com>
2024-08-13 23:33:01 +0530

    Refresh tokens

--- BackEnd/src/models/user.model.js
+++ BackEnd/src/models/user.model.js
@@ -62,5 +62,14 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
-
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_EXPIRE,
+  });
+};
 export const User = mongoose.model("User", userSchema);
